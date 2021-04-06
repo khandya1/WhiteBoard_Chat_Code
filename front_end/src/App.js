2393cae9b0f1e9e754aa48d0030051a84bf6ad58
rajbhatti <rajcbhatti@gmail.com>
2021-04-06 19:37:32 +0530

    Added basic whiteboard component.

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,16 +1,36 @@
-import React, { Component, Fragment } from 'react'
-import Editor from './components/Editor'
+import React, { Component, Fragment } from "react";
+import Editor from "./components/Editor";
+import Navbar from "./components/Navbar";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import ThinkPad from "./components/ThinkPad";
 
+var Sketchpad = require("responsive-sketchpad");
 
 class App extends Component {
-
   render() {
     return (
       <Fragment>
-        <Editor />
+        <Navbar />
+        <div
+          style={{
+            backgroundColor: "#f1f3f8",
+            fontFamily: "poppins",
+            padding: "50px",
+          }}
+        >
+          <Grid container spacing={5}>
+            <Grid item xs={12} sm={12} md={6}>
+              <Editor />
+            </Grid>
+            <Grid item xs={12} sm={12} md={6}>
+              <ThinkPad />
+            </Grid>
+          </Grid>
+        </div>
       </Fragment>
-    )
+    );
   }
 }
 
-export default App
+export default App;
