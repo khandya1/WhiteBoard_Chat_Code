da8508df35fd14f6742b950c8d4888b77e38694f
khandya1 <63929895+khandya1@users.noreply.github.com>
2021-04-19 14:36:36 +0530

    code editor added

--- front_end/src/components/Editor.jsx
+++ front_end/src/components/Editor.jsx
@@ -1,34 +1,49 @@
 import React, { Component } from "react";
-import "ace-builds";
 import AceEditor from "react-ace";
-import "ace-builds/webpack-resolver";
 import "ace-builds/src-min-noconflict/ext-searchbox";
 import "ace-builds/src-min-noconflict/ext-language_tools";
 import "ace-builds/src-noconflict/mode-jsx";
+import {
+  FormControlLabel,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+} from "@material-ui/core";
+import ShareIcon from "@material-ui/icons/Share";
+import Switch from "@material-ui/core/Switch";
+
+// For resize...
+import ReactResizeDetector from "react-resize-detector";
 
 const languages = [
-  "javascript",
+  "c_cpp",
   "java",
   "python",
-  "xml",
+  "javascript",
+  "html",
   "ruby",
-  "sass",
+  "typescript",
+  "kotlin",
+  "swift",
   "markdown",
   "mysql",
   "json",
-  "html",
-  "handlebars",
   "golang",
   "csharp",
-  "elixir",
-  "typescript",
-  "css",
+  "sql",
+  "sqlserver",
 ];
 
 const themes = [
   "monokai",
   "github",
   "tomorrow",
+  "tomorrow_night",
   "kuroir",
   "twilight",
   "xcode",
@@ -38,15 +53,34 @@ const themes = [
   "terminal",
 ];
 
+//extracting all the languages recquired
 languages.forEach((lang) => {
   require(`ace-builds/src-noconflict/mode-${lang}`);
   require(`ace-builds/src-noconflict/snippets/${lang}`);
 });
-
+//extracting themes
 themes.forEach((theme) => require(`ace-builds/src-noconflict/theme-${theme}`));
-/*eslint-disable no-alert, no-console */
 
-const defaultValue = "Welcome";
+//default value to be displayed on code editor
+const defaultValue = `#include <bits/stdc++.h>
+#define lli long long int
+#define endl "\\n"
+#define MAX 1000005
+#define MOD 1000000007
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    //your code
+	  
+	}
+	return 0;
+}`;
+
 class Editor extends Component {
   onChange(newValue) {
     console.log("change", newValue);
@@ -74,42 +108,60 @@ class Editor extends Component {
       placeholder: e.target.value,
     });
   }
+
   setTheme(e) {
     this.setState({
       theme: e.target.value,
     });
   }
+
   setMode(e) {
     this.setState({
       mode: e.target.value,
     });
   }
+
   setBoolean(name, value) {
     this.setState({
       [name]: value,
     });
   }
+
   setFontSize(e) {
     this.setState({
       fontSize: parseInt(e.target.value, 10),
     });
   }
+
+  // for resize...------------------------
+  onResize(w, h) {
+    this.setState({
+      editorHeight: h,
+      editorWidth: w,
+    });
+  }
+
   constructor(props) {
     super(props);
     this.state = {
       value: defaultValue,
       placeholder: "Placeholder Text",
-      theme: "monokai",
-      mode: "javascript",
+      theme: "tomorrow_night",
+      mode: "c_cpp",
       enableBasicAutocompletion: false,
       enableLiveAutocompletion: false,
-      fontSize: 14,
+      fontSize: 16,
       showGutter: true,
       showPrintMargin: true,
       highlightActiveLine: true,
       enableSnippets: false,
       showLineNumbers: true,
+      editorHeight: 400,
+      editorWidth: "auto",
     };
+
+    //for resize...
+    this.onResize = this.onResize.bind(this);
     this.setPlaceholder = this.setPlaceholder.bind(this);
     this.setTheme = this.setTheme.bind(this);
     this.setMode = this.setMode.bind(this);
@@ -117,84 +169,177 @@ class Editor extends Component {
     this.setFontSize = this.setFontSize.bind(this);
     this.setBoolean = this.setBoolean.bind(this);
   }
+
   render() {
     return (
       <div className="columns">
-        <div className="column">
-          <div className="field">
-            <label>Mode:</label>
-            <p className="control">
-              <span className="select">
-                <select
-                  name="mode"
-                  onChange={this.setMode}
-                  value={this.state.mode}
-                >
-                  {languages.map((lang) => (
-                    <option key={lang} value={lang}>
-                      {lang}
-                    </option>
-                  ))}
-                </select>
-              </span>
-            </p>
-          </div>
+        <AppBar position="static" style={{ backgroundColor: "#393b44" }}>
+          <Toolbar>
+            <Typography
+              variant="h5"
+              style={{ fontFamily: "poppins", color: "#f1f3f8" }}
+            >
+              CodeEditor &nbsp;&nbsp;&nbsp;&nbsp;
+            </Typography>
 
-          <div className="field">
-            <label>Theme:</label>
-            <p className="control">
-              <span className="select">
-                <select
-                  name="Theme"
-                  onChange={this.setTheme}
-                  value={this.state.theme}
-                >
-                  {themes.map((lang) => (
-                    <option key={lang} value={lang}>
-                      {lang}
-                    </option>
-                  ))}
-                </select>
-              </span>
-            </p>
-          </div>
+            <FormControl
+              variant="outlined"
+              style={{
+                width: "120px",
+                marginTop: "10px",
+              }}
+            >
+              <InputLabel
+                id="mode-label"
+                style={{ fontFamily: "poppins", color: "#ffffff" }}
+              >
+                Language
+              </InputLabel>
+              <Select
+                name="mode"
+                labelId="mode-label"
+                id="select-mode"
+                value={this.state.mode}
+                onChange={this.setMode}
+                style={{
+                  fontFamily: "poppins",
+                  color: "#ffffff",
+                  fontSize: "0.8rem",
+                  height: "35px",
+                }}
+              >
+                {languages.map((lang) => (
+                  <MenuItem value={lang} key={lang}>
+                    {lang.toUpperCase()}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <FormControl
+              variant="outlined"
+              style={{
+                width: "120px",
+                marginTop: "10px",
+                marginLeft: "5px",
+              }}
+            >
+              <InputLabel
+                id="theme-label"
+                style={{ fontFamily: "poppins", color: "#ffffff" }}
+              >
+                Theme
+              </InputLabel>
+              <Select
+                name="Theme"
+                labelId="theme-label"
+                id="select-theme"
+                onChange={this.setTheme}
+                value={this.state.theme}
+                style={{
+                  fontFamily: "poppins",
+                  color: "#ffffff",
+                  fontSize: "0.8rem",
+                  height: "35px",
+                }}
+              >
+                {themes.map((lang) => (
+                  <MenuItem key={lang} value={lang}>
+                    {lang}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
 
-          <div className="field">
-            <label>Font Size:</label>
-            <p className="control">
-              <span className="select">
-                <select
-                  name="Font Size"
-                  onChange={this.setFontSize}
-                  value={this.state.fontSize}
-                >
-                  {[14, 16, 18, 20, 24, 28, 32, 40].map((lang) => (
-                    <option key={lang} value={lang}>
-                      {lang}
-                    </option>
-                  ))}
-                </select>
-              </span>
-            </p>
-          </div>
+            <FormControl
+              variant="outlined"
+              style={{
+                width: "120px",
+                marginTop: "10px",
+                marginLeft: "5px",
+              }}
+            >
+              <InputLabel
+                id="font-label"
+                style={{ fontFamily: "poppins", color: "#ffffff" }}
+              >
+                Font Size
+              </InputLabel>
+              <Select
+                name="Theme"
+                labelId="font-label"
+                id="select-font"
+                onChange={this.setFontSize}
+                value={this.state.fontSize}
+                style={{
+                  fontFamily: "poppins",
+                  color: "#ffffff",
+                  fontSize: "0.8rem",
+                  height: "35px",
+                }}
+              >
+                {[14, 16, 18, 20, 24, 28, 32, 40].map((lang) => (
+                  <MenuItem key={lang} value={lang}>
+                    {lang}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <Button
+              variant="contained"
+              startIcon={<ShareIcon />}
+              style={{
+                fontFamily: "poppins",
+                marginLeft: "auto",
+                fontWeight: "600",
+                color: "#f1f3f8",
+                backgroundColor: "#99A3CD",
+              }}
+            >
+              Share
+            </Button>
+          </Toolbar>
+        </AppBar>
 
-          <div className="field">
-            <label>Placeholder:</label>
-            <p className="control">
-              <input
-                className="input"
-                type="text"
-                onChange={this.setPlaceholder}
-                value={this.state.placeholder}
-              />
-            </p>
-          </div>
+        <ReactResizeDetector
+          handleWidth
+          handleHeight
+          onResize={this.onResize}
+        />
 
-          <div className="field">
-            <p className="control">
-              <label className="checkbox">
-                <input
-                  type="checkbox"
+        <AceEditor
+          placeholder={this.state.placeholder}
+          mode={this.state.mode}
+          theme={this.state.theme}
+          name="blah2"
+          height="550px"
+          width={this.state.editorWidth}
+          onLoad={this.onLoad}
+          onChange={this.onChange}
+          onSelectionChange={this.onSelectionChange}
+          onCursorChange={this.onCursorChange}
+          onValidate={this.onValidate}
+          value={this.state.value}
+          fontSize={this.state.fontSize}
+          showPrintMargin={this.state.showPrintMargin}
+          showGutter={this.state.showGutter}
+          highlightActiveLine={this.state.highlightActiveLine}
+          setOptions={{
+            useWorker: false,
+            enableBasicAutocompletion: this.state.enableBasicAutocompletion,
+            enableLiveAutocompletion: this.state.enableLiveAutocompletion,
+            enableSnippets: this.state.enableSnippets,
+            showLineNumbers: this.state.showLineNumbers,
+            tabSize: 2,
+          }}
+        />
+
+        <AppBar position="static" style={{ backgroundColor: "#393b44" }}>
+          <Toolbar>
+            &nbsp;&nbsp;&nbsp;
+            <FormControlLabel
+              control={
+                <Switch
+                  size="small"
                   checked={this.state.enableBasicAutocompletion}
                   onChange={(e) =>
                     this.setBoolean(
@@ -202,126 +347,87 @@ class Editor extends Component {
                       e.target.checked
                     )
                   }
+                  color="primary"
                 />
-                Enable Basic Autocomplete
-              </label>
-            </p>
-          </div>
-          <div className="field">
-            <p className="control">
-              <label className="checkbox">
-                <input
-                  type="checkbox"
-                  checked={this.state.enableLiveAutocompletion}
-                  onChange={(e) =>
-                    this.setBoolean(
-                      "enableLiveAutocompletion",
-                      e.target.checked
-                    )
-                  }
-                />
-                Enable Live Autocomplete
-              </label>
-            </p>
-          </div>
-          <div className="field">
-            <p className="control">
-              <label className="checkbox">
-                <input
-                  type="checkbox"
+              }
+              label="Enable Basic Autocomplete&nbsp;&nbsp;&nbsp;"
+            />
+            <FormControlLabel
+              control={
+                <Switch
+                  size="small"
                   checked={this.state.showGutter}
                   onChange={(e) =>
                     this.setBoolean("showGutter", e.target.checked)
                   }
+                  color="primary"
                 />
-                Show Gutter
-              </label>
-            </p>
-          </div>
-          <div className="field">
-            <p className="control">
-              <label className="checkbox">
-                <input
-                  type="checkbox"
+              }
+              label="Show Gutter&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+            />
+            <FormControlLabel
+              control={
+                <Switch
+                  size="small"
                   checked={this.state.showPrintMargin}
                   onChange={(e) =>
                     this.setBoolean("showPrintMargin", e.target.checked)
                   }
+                  color="primary"
                 />
-                Show Print Margin
-              </label>
-            </p>
-          </div>
-          <div className="field">
-            <p className="control">
-              <label className="checkbox">
-                <input
-                  type="checkbox"
-                  checked={this.state.highlightActiveLine}
-                  onChange={(e) =>
-                    this.setBoolean("highlightActiveLine", e.target.checked)
-                  }
-                />
-                Highlight Active Line
-              </label>
-            </p>
-          </div>
-          <div className="field">
-            <p className="control">
-              <label className="checkbox">
-                <input
-                  type="checkbox"
-                  checked={this.state.enableSnippets}
-                  onChange={(e) =>
-                    this.setBoolean("enableSnippets", e.target.checked)
-                  }
-                />
-                Enable Snippets
-              </label>
-            </p>
-          </div>
-          <div className="field">
-            <p className="control">
-              <label className="checkbox">
-                <input
-                  type="checkbox"
-                  checked={this.state.showLineNumbers}
-                  onChange={(e) =>
-                    this.setBoolean("showLineNumbers", e.target.checked)
-                  }
-                />
-                Show Line Numbers
-              </label>
-            </p>
-          </div>
-        </div>
-        <div className="examples column">
-          <h2>Editor</h2>
-          <AceEditor
-            placeholder={this.state.placeholder}
-            mode={this.state.mode}
-            theme={this.state.theme}
-            name="blah2"
-            onLoad={this.onLoad}
-            onChange={this.onChange}
-            onSelectionChange={this.onSelectionChange}
-            onCursorChange={this.onCursorChange}
-            onValidate={this.onValidate}
-            value={this.state.value}
-            fontSize={this.state.fontSize}
-            showPrintMargin={this.state.showPrintMargin}
-            showGutter={this.state.showGutter}
-            highlightActiveLine={this.state.highlightActiveLine}
-            setOptions={{
-              useWorker: false,
-              enableBasicAutocompletion: this.state.enableBasicAutocompletion,
-              enableLiveAutocompletion: this.state.enableLiveAutocompletion,
-              enableSnippets: this.state.enableSnippets,
-              showLineNumbers: this.state.showLineNumbers,
-              tabSize: 2,
-            }}
-          />
-        </div>
+              }
+              label="Show Print Margin"
+            />
+          </Toolbar>
+          <Toolbar>
+            <div className="field">
+              <p className="control">
+                <label className="checkbox">
+                  <Switch
+                    size="small"
+                    checked={this.state.highlightActiveLine}
+                    onChange={(e) =>
+                      this.setBoolean("highlightActiveLine", e.target.checked)
+                    }
+                    color="primary"
+                  />
+                  Highlight Active Line&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                </label>
+              </p>
+            </div>
+            <div className="field">
+              <p className="control">
+                <label className="checkbox">
+                  <Switch
+                    size="small"
+                    checked={this.state.enableSnippets}
+                    onChange={(e) =>
+                      this.setBoolean("enableSnippets", e.target.checked)
+                    }
+                    color="primary"
+                  />
+                  Enable Snippets&nbsp;&nbsp;
+                </label>
+              </p>
+            </div>
+            <div className="field">
+              <p className="control">
+                <label className="checkbox">
+                  <Switch
+                    size="small"
+                    checked={this.state.showLineNumbers}
+                    onChange={(e) =>
+                      this.setBoolean("showLineNumbers", e.target.checked)
+                    }
+                    color="primary"
+                  />
+                  Show Line Numbers
+                </label>
+              </p>
+            </div>
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
