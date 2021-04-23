61b5340a78dadfebca7173b829ecaaef652c11b6
rajbhatti <rajcbhatti@gmail.com>
2021-04-23 18:46:21 +0530

    Added HomePage

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,38 +1,18 @@
-import React, { Component, Fragment } from "react";
-import Editor from "./components/Editor";
-import Navbar from "./components/Navbar";
-import { makeStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
-import WhiteBoard from "./components/WhiteBoard";
-import SyntaxEditor from './components/SyntaxEditor';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import SyntaxRoom from "./components/SyntaxRoom/SyntaxRoom";
 import "fontsource-poppins";
 
-var Sketchpad = require("responsive-sketchpad");
-
-class App extends Component {
-  render() {
-    return (
-      <Fragment>
-        <Navbar />
-        <div
-          style={{
-            backgroundColor: "#f1f3f8",
-            fontFamily: "poppins",
-            padding: "50px",
-          }}
-        >
-          <Grid container spacing={5}>
-            <Grid item xs={12} sm={12} md={6}>
-              <SyntaxEditor />
-            </Grid>
-            <Grid item xs={12} sm={12} md={6}>
-              <WhiteBoard />
-            </Grid>
-          </Grid>
-        </div>
-      </Fragment>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/:roomId" component={SyntaxRoom} />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
